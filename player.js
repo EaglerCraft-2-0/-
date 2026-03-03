eed8348b59bd721510cf2d3e65f7a7e5152db501
umutstudio <baharelidenk@gmail.com>
2026-03-03 21:30:26 +0300

    Update player.js

--- player.js
+++ player.js
@@ -1,3 +1,22 @@
 export function initPlayer(camera){
-camera.position.set(0,50,50);
+
+    camera.position.set(0,40,0);
+
+    let velocityY = 0;
+    const gravity = -0.02;
+
+    function update(){
+
+        velocityY += gravity;
+        camera.position.y += velocityY;
+
+        if(camera.position.y < 30){
+            camera.position.y = 30;
+            velocityY = 0;
+        }
+
+        requestAnimationFrame(update);
+    }
+
+    update();
 }
